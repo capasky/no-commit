7523b145c8a111f6a665d5225f8e21eff2c2563f
yellhb@gmail.com <yellhb@gmail.com@576725e3-fa23-f7cf-1677-9ee96f47fef8>
2012-12-15 01:23:19 +0000

    修改TCPListener.h中socket->SOCKET 实现TCPListener.c

--- api/inet/TCPListener.c
+++ api/inet/TCPListener.c
@@ -16,37 +16,58 @@
  * yellhb               2012.12.14          1.0.0.1
  */
 
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "TCPListener.h"
+#include "inetdef.h"
 
 /**
-  * TCPListener_Create 创建一个TCPListener结构体对象
-  */
-TCPListener * TCPListen_Create ( char * ipAddress, int port )
+ * TCPListener_Create 创建一个TCPClient结构体对象
+ * @param ipAddress 指示TCP监听服务端的IP地址
+ * @param port 指示端口号
+ * @return 成功则返回创建的TCPListener结构体的指针，否则返回NULL
+ */
+TCPListener * TCPListener_Create(char * ipAddress, int port)
 {
-	
+	TCPListener *listener = ( TCPListener * ) malloc ( sizeof ( TCPListener ));
+	listener->Active = false;
+	strcpy ( listener->IPAddress, ipAddress );
+	listener->Server = -1;
+	listener->Port = 0;
+
+	return listener;
 }
 
 /**
-  * TCPListener_Accept 接受挂起到连接请求
-  */
-socket TCPListener_Accept ( TCPListener * listener )
+ * TCPListener_Accept 接受挂起的连接请求。
+ * @param listener TCPListener结构体对象指针。
+ * @return 成功则返回创建的与客户端通信的SOCKET，否则返回空的SOCKET。
+ */
+SOCKET TCPListener_Accept(TCPListener * listener)
 {
-
+	return 0;
 }
 
 /**
-  * TCPListener_Start 启动对具有最大挂起连接数的传入连接请求到侦听
-  */
-bool TCPListener_Start ( TCPListener * listener, int backlog )
+ * TCPListener_Start 启动对具有最大挂起连接数的传入连接请求的侦听。
+ * @param listener TCPListener结构体对象指针。
+ * @param backlog 挂起连接队列的最大长度。
+ * @return 成功则返回true，否则返回false
+ */
+bool TCPListener_Start(TCPListener * listener, int backlog)
 {
-
+	return listen ( listener->Server, backlog );
 }
 
 /**
-  * TCPListener_Stop 关闭侦听器
-  */
-bool TCPListener_Stop ( TCPListener * listener )
+ * TCPListener_Stop 关闭侦听器。
+ * @param listener TCPListener结构体对象指针。
+ * @return 成功则返回true，否则返回false
+ */
+bool TCPListener_Stop(TCPListener * listener)
 {
-	
+	return close ( listener->Server );
 }
 
