acfa00bbd445f57bd96c4fc3fcb22f7608b80d56
yellhb@gmail.com <yellhb@gmail.com@576725e3-fa23-f7cf-1677-9ee96f47fef8>
2012-12-19 04:38:41 +0000

    修改TCPServer

--- trunk/api/inet/TCPServer.c
+++ trunk/api/inet/TCPServer.c
@@ -32,15 +32,19 @@
   */
 TCPServer * TCPServer_Create ( char *ipAddress, int port )
 {
-	TCPServer *tcpServer = ( TCPServer * ) malloc ( sizeof (TCPServer ));
+	TCPServer *tcpServer = ( TCPServer* ) malloc ( sizeof (TCPServer ));
+	
 	tcpServer->sockfd = socket ( PF_INET, SOCK_STREAM, 0);
 	tcpServer->Port = port;
 	strcpy ( tcpServer->IPAddress, ipAddress );
+	
 	tcpServer->addr.sin_family = AF_INET;
 	tcpServer->addr.sin_port = htons ( port );
 	tcpServer->addr.sin_addr.s_addr = inet_addr ( tcpServer->IPAddress );
 	memset ( &(tcpServer->addr.sin_zero), 0, 8 );
 
+	tcpServer->clientaddr_len = sizeof ( struct sockaddr_in );
+	
 	return tcpServer;
 }
 
@@ -52,7 +56,7 @@ TCPServer * TCPServer_Create ( char *ipAddress, int port )
 int TCPServer_Bind ( TCPServer *tcpServer )
 {
 	return  bind ( tcpServer->sockfd, 
-				 ( struct sockaddr * ) &(tcpServer->addr), 
+				 ( struct sockaddr* ) &(tcpServer->addr), 
 				 sizeof ( struct sockaddr ));
 }
 
@@ -64,5 +68,53 @@ int TCPServer_Bind ( TCPServer *tcpServer )
 int TCPServer_Close ( TCPServer *tcpServer )
 {
 	return close ( tcpServer->sockfd );
+}
+
+/**
+  * TCPServer_Recv 接收客户端消息
+  * @param sockfd
+  * @return 返回收到到消息串，若接收失败返回NULL
+  */
+char * TCPServer_Recv ( SOCKET sockfd )
+{
+	char * recvBuf = ( char* ) malloc ( sizeof ( char ) * MAX_BUF_SIZE );
+	
+	if ( recv ( sockfd, recvBuf, MAX_BUF_SIZE, 0) > 0 )
+		return recvBuf;
+	else 
+		return NULL;
+}
+
+/**
+  * TCPServer_Send 向客户端发送反馈消息
+  * @param sockfd
+  * @param 待发送到消息序列
+  * @return 返回发送到字节总数
+  */
+int TCPServer_Send ( SOCKET sockfd, char * sendBuf )
+{
+	return send ( sockfd, sendBuf, strlen ( sendBuf ), 0 );
+}	
+
+/**
+  * TCPServer_Listen 使server到socket处于被动监听状态，并设置消息队列
+  * @param TCPServer指针
+  * @return 返回设置是否成功 -1为失败，0为成功
+  */
+int TCPServer_Listen ( TCPServer *tcpServer )
+{
+	return listen ( tcpServer->sockfd, MAX_CONNECT_QUEUE );
+}
+
+/**
+  * TCPServer_Accept 接收客户端请求
+  * @param TCPServer指针
+  * @return 返回新到socket标识
+  */
+SOCKET TCPServer_Accept ( TCPServer *tcpServer )
+{
+	return accept ( tcpServer->sockfd,
+			( struct sockaddr* ) & ( tcpServer->clientaddr ),
+			& ( tcpServer->clientaddr_len ));
 }
 
