6c39d7c55f40bc50b376f52b21791c578bd98dd5
ikeo88@gmail.com <ikeo88@gmail.com@576725e3-fa23-f7cf-1677-9ee96f47fef8>
2013-01-12 06:37:31 +0000

    修正Updater服务器更新Bug

--- trunk/client.c
+++ trunk/client.c
@@ -16,14 +16,24 @@
  * capasky              2012.12.08          1.0.0.1
  * capasky				2012.12.26			1.1.0.0
  */
+#include <stdlib.h>
+#include <string.h>
 
 #include "ncclient.h"
 
-NCClient * NCC;
+NCClient * NCC = NULL;
 
-int main()
+int main(int argc, char ** argv)
 {
 	NCC = NCClient_Create();
+	if (argc > 1)
+	{
+		strcpy(NCC->UpdateServerIP, argv[1]);
+	}
+	if (argc > 2)
+	{
+		NCC->UpdateServerPort = atoi(argv[2]);
+	}
 	NCClient_Run(NCC);
 	return 0;
 }
